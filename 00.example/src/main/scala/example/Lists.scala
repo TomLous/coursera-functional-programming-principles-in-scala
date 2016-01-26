3665fee2881502bd23091d6d966c7c81d56393fd
Tom Lous <tomlous@gmail.com>
2016-01-26 21:52:27 +0100

    Fixed assertions and implemented Lists methods

--- 00.example/src/main/scala/example/Lists.scala
+++ 00.example/src/main/scala/example/Lists.scala
@@ -23,7 +23,10 @@ object Lists {
    * @param xs A list of natural numbers
    * @return The sum of all elements in `xs`
    */
-  def sum(xs: List[Int]): Int = ???
+  def sum(xs: List[Int]): Int = {
+    if(xs.isEmpty) 0
+    else xs.head + sum(xs.tail)
+  }
 
   /**
    * This method returns the largest element in a list of integers. If the
@@ -38,5 +41,17 @@ object Lists {
    * @return The largest element in `xs`
    * @throws java.util.NoSuchElementException if `xs` is an empty list
    */
-  def max(xs: List[Int]): Int = ???
+  def max(xs: List[Int]): Int = {
+    if(xs.isEmpty){
+       throw new java.util.NoSuchElementException
+    }
+    else
+    {
+      def maxRemaining(currentMax: Int, xss:List[Int]): Int = {
+        if(xss.isEmpty) currentMax
+        else maxRemaining(Math.max(currentMax, xss.head), xss.tail)
+      }
+      maxRemaining(xs.head, xs.tail)
+    }
+  }
 }
